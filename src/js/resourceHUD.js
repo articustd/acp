5569772199e15f0e7383d6eaeba13ba6a88e028e
Articus <articusthedata@gmail.com>
2022-12-02 14:17:37 -0800

    Cleaned up hud

--- src/js/resourceHUD.js
+++ src/js/resourceHUD.js
@@ -2,31 +2,34 @@ import { getScene } from "@GameEngine/Core"
 import { logger } from "@util/Logging"
 
 export function showHUD() {
+    let story = getScene('EventInteraction').story
     let $container = $('<div/>').attr('id', 'hud')
-    let $menuButton = $('<div/>').wiki(`Menu`).addClass('menuButton').addClass('button')
+    let $menuButton = $('<div/>').append('<i class="fa fa-bars" aria-hidden="true"/>').addClass('button menu-button').attr('title', 'Dev Menu')
+    let $saveMenuButton = $('<div/>').append('<i class="fa fa-floppy-o" aria-hidden="true"/>').addClass('button menu-button').attr('title', 'Save/Load Game')
+    let $restartMenuButton = $('<div/>').append('<i class="fa fa-power-off" aria-hidden="true"/>').addClass('button menu-button').attr('title', 'Restart Game')
+    let $saveStoryMenuButton = $('<div/>').append('<i class="fa fa-book" aria-hidden="true"/>').addClass('button menu-button').attr('title', 'Export Story')
 
-    let $saveButton = createMenuItem(`Saves`)
-    let $restartButton = createMenuItem(`Restart`)
+    $saveMenuButton.click(() => { Dialog.close(); UI.saves(); })
+    $restartMenuButton.click(() => { Dialog.close(); UI.restart(); })
+    $saveStoryMenuButton.click(() => {
+        let blob = new Blob([formatStory(story.storySnippets)], { type: "text/plain;charset=utf-8" });
+        saveAs(blob, 'story.txt')
+    })
 
     let $tickerEditorButton = createMenuItem(`Ticker Editor`)
     let $eventTestButton = createMenuItem(`Event Test`)
     let $drawflowButton = createMenuItem(`Drawflow`)
 
     let $menuList = $('<ul/>').addClass('menu-list')
-        .append($saveButton)
-        .append($restartButton)
     let $menuVersion = $('<div/>').addClass('menu-version').wiki(variables().version)
 
-    if (variables().dev)
-        $menuList.append($tickerEditorButton)
-            // .append($eventTestButton)
-            .append($drawflowButton)
+    $menuList
+        .append($tickerEditorButton)
+        // .append($eventTestButton)
+        .append($drawflowButton)
 
     $menuButton.click(() => {
         Dialog.setup('Menu')
-
-        $saveButton.click(() => { Dialog.close(); UI.saves(); })
-        $restartButton.click(() => { Dialog.close(); UI.restart(); })
 
         if (variables().dev)
             devButtons()
@@ -42,14 +45,24 @@ export function showHUD() {
         $eventTestButton.click(() => { Dialog.close(); Engine.play("eventScenario") })
         $drawflowButton.click(() => { Dialog.close(); Engine.play("drawflow") })
     }
-    
+
     // $container.append($('<div/>').wiki(`''Kobold''<div><<koboldAvailableCounter>>/<<koboldPopCounter>></div>`).addClass('hudItem'))
-    $container.append($('<div/>').wiki('Am Empty Now').addClass('hudItem'))
-    $container.append($menuButton)
+    $container.append($('<div/>').wiki(`''?EventName''~~?EventVersion~~`).addClass('hudItem'))
+    $container
+        .append($saveStoryMenuButton)
+        .append($saveMenuButton)
+        .append($restartMenuButton)
+
+    if (variables().dev)
+        $container.append($menuButton)
 
     $('#passages').before($container)
 }
 
 function createMenuItem(text) {
     return $('<li/>').append($('<div/>').addClass('button').wiki(text))
+}
+
+function formatStory(storySnippets) {
+    return _.join(storySnippets, '\n')
 }
--- src/js/template/resourceTemplate.js
+++ src/js/template/resourceTemplate.js
@@ -3,3 +3,7 @@ import { getScene } from "@GameEngine/Core";
 Template.add('EventName', function() {
     return getScene('EventInteraction').eventName
 })
+
+Template.add('EventVersion', function() {
+    return variables().version
+})
